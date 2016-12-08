281b74d95f021d7c345313bc89f69228a0862fed
ogusu <xogusu.takuya@contract.isid.co.jp>
2016-12-08 13:38:24 +0900

    test

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sudo apt-get update -qq
-sudo apt-get install -qq libssh2-1-dev openssh-client openssh-server
+apt-get update -qq
+apt-get install -qq libssh2-1-dev openssh-client openssh-server
 
-sudo start ssh
+start ssh
 
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -N "" -q
 cat ~/.ssh/id_rsa.pub >>~/.ssh/authorized_keys
